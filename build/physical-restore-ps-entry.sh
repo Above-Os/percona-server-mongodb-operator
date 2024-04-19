0abae0e8f5b17e53dbcb0ce3114d7134f89a62dc
aby913 <aby913@163.com>
2024-04-19 13:48:56 +0800

    fix: restore

--- build/physical-restore-ps-entry.sh
+++ build/physical-restore-ps-entry.sh
@@ -3,9 +3,9 @@
 set -Eeuo pipefail
 set -o xtrace
 
-log=/tmp/pbm-agent-restore.log
+log=/tmp/pbm-agent.log
 
-/opt/percona/pbm-agent-restore 2> ${log} &
+/opt/percona/pbm-agent 2> ${log} &
 /opt/percona/ps-entry.sh "$@"
 
 echo "Physical restore in progress"
